63358ab7b44558cb7d0cf80eba5e9b3b4e4c38fb
Johan Camilo Cortes Ocampo <camilo4182@gmail.com>
2022-07-28 16:53:44 -0500

    Setup project

---
+++ src/test/java/com/bootcamp/rest/webservices/SpringBootRestfulWebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.rest.webservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestfulWebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
